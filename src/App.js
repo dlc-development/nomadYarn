940e6a63c5702239d91df3992395de50406727bd
dlc-development <32686716+dlc-development@users.noreply.github.com>
2023-12-06 11:31:11 -0800

    Test Routing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Router, Link, Redirect } from "@reach/router";
 const Test = lazy(() => import('./Components/Test'));
+const Test2 = lazy(() => import('./Components/Test2'));
 
 
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -22,9 +24,13 @@ function App() {
         >
           Learn React
         </a>
-        <Test />
+        <Router>
+          <Test path='/' />
+          <Test2 path='/asd' />
+        </Router>
       </header>
     </div>
+    
   );
 }
 
---
+++ src/Components/Test2.js
@@ -0,0 +1,21 @@
+
+import { Button, Container, Row, Col, Image,Card} from 'react-bootstrap';
+
+const Test2 = () => {
+    return(
+        
+        <Container>
+          <Row>
+            <Col>
+            <Card>
+            <Button>
+                Test 2
+                </Button>
+            </Card>
+            </Col>
+          </Row>
+        </Container>
+    )
+}
+
+export default Test2;
