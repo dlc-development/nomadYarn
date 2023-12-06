25745ade6df5e52ed0035ba71278317299075b63
dlc-development <32686716+dlc-development@users.noreply.github.com>
2023-12-06 11:05:44 -0800

    Added Bootstrap Elements

---
+++ src/Components/Test.js
@@ -0,0 +1,20 @@
+import { Button, Container, Row, Col, Image,Card} from 'react-bootstrap';
+
+const Test = () => {
+    return(
+        
+        <Container>
+          <Row>
+            <Col>
+            <Card>
+            <Button>
+                Daves Button
+                </Button>
+            </Card>
+            </Col>
+          </Row>
+        </Container>
+    )
+}
+
+export default Test;
