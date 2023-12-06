5f5ef4033e5e831ff5c6e87bd5a165fccc846087
dlc-development <32686716+dlc-development@users.noreply.github.com>
2023-12-06 11:45:51 -0800

    Lazy Helmet Test

--- src/App.js
+++ src/App.js
@@ -4,33 +4,39 @@ import './App.css';
 import { Router, Link, Redirect } from "@reach/router";
 const Test = lazy(() => import('./Components/Test'));
 const Test2 = lazy(() => import('./Components/Test2'));
+const Helmet = lazy(() => import('react-helmet-async'));
+const HelmetProvider = lazy(() => import('react-helmet-async'));
 
 
 
 function App() {
   return (
-    
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Router>
-          <Test path='/' />
-          <Test2 path='/asd' />
-        </Router>
-      </header>
-    </div>
-    
+    <HelmetProvider>
+      <Helmet>
+        <title>Education Loans for International Students USA</title>
+
+      </Helmet>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            <Router>
+              <Test path='/' />
+              <Test2 path='/asd' />
+            </Router>
+          </header>
+        </div>
+    </HelmetProvider>
   );
 }
 
